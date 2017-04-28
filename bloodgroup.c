a4145b9e4049d5aee3e737c49d376accc2925ff8
rapeepap <ismileutube@gmail.com>
2017-04-28 11:23:56 +0700

    Change 1 file

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[100], G[10], answer[10];
+{ char Name[50], G[10], answer[10];
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("tell me your name ");
     scanf("%s", Name);
     do
     {
